ba3e62faf538ab9ed2699bedcdece7082ba6a90a
TheDragonHuaQi <1684214048@qq.com>
2026-08-17 21:06:54 +0800

    为烟火凡人心的所有种子添加了节气的标签

---
+++ kubejs/server_scripts/flavor_immersed_daily_seeds.js
@@ -0,0 +1,147 @@
+// 监听物品标签事件
+ServerEvents.tags('item', event => {
+    // 为种子添加两个标签
+    event.add('eclipticseasons:crops/average_humid', 'flavor_immersed_daily:chineseleavesseed');
+    event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:chineseleavesseed');
+	
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:radishseed');
+    event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:radishseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:white_mushroom_seed');
+    event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:white_mushroom_seed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:blackfungsseed');
+    event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:blackfungsseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:pleurotusseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:pleurotusseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:enokimushroomseed');
+	event.add('eclipticseasons:crops/autumn_winter', 'flavor_immersed_daily:enokimushroomseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:tremellaseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:tremellaseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:fragrantseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:fragrantseed');
+	
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:blueberryseed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:blueberryseed');
+	
+	event.add('eclipticseasons:crops/average_humid', 'flavor_immersed_daily:dragonfruitseed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:dragonfruitseed');
+	
+	event.add('eclipticseasons:crops/average_humid', 'flavor_immersed_daily:greentealeavesseed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:greentealeavesseed');
+	
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:hamimelonseed');
+	event.add('eclipticseasons:crops/spring_summer', 'flavor_immersed_daily:hamimelonseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:pineappleseed');
+	event.add('eclipticseasons:crops/all_seasons', 'flavor_immersed_daily:pineappleseed');
+	
+	event.add('eclipticseasons:crops/average_humid', 'flavor_immersed_daily:red_tea_seed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:red_tea_seed');
+	
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:strawberryseed');
+	event.add('eclipticseasons:crops/spring_summer', 'flavor_immersed_daily:strawberryseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:lotusrootseed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:lotusrootseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:broccoilseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:broccoilseed');
+	
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:buckwheatseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:buckwheatseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:cabbageseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:cabbageseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:cassavaseeds');
+	event.add('eclipticseasons:crops/all_seasons', 'flavor_immersed_daily:cassavaseeds');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:celeryseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:celeryseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:chinesechivesseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:chinesechivesseed');
+	
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:chineseyamseed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:chineseyamseed');
+	
+	event.add('eclipticseasons:crops/average_humid', 'flavor_immersed_daily:coffeebeanseed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:coffeebeanseed');
+	
+	event.add('eclipticseasons:crops/dry_average', 'flavor_immersed_daily:cuminseed');
+	event.add('eclipticseasons:crops/spring_summer', 'flavor_immersed_daily:cuminseed');
+	
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:fennelseedstates');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:fennelseedstates');
+	
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:garlicseed');
+	event.add('eclipticseasons:crops/autumn_winter', 'flavor_immersed_daily:garlicseed');
+
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:ginger_seed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:ginger_seed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:greenpepperseeds');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:greenpepperseeds');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:gumboseed');
+	event.add('eclipticseasons:crops/spring_summer', 'flavor_immersed_daily:gumboseed');
+
+	event.add('eclipticseasons:crops/average_humid', 'flavor_immersed_daily:mustrad_seed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:mustrad_seed');
+
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:nutmegseed');
+	event.add('eclipticseasons:crops/all_seasons', 'flavor_immersed_daily:nutmegseed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:oatseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:oatseed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:oilrapeseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:oilrapeseed');
+
+	event.add('eclipticseasons:crops/dry_average', 'flavor_immersed_daily:onionseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:onionseed');
+
+	event.add('eclipticseasons:crops/average_humid', 'flavor_immersed_daily:peaseed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:peaseed');
+	
+	event.add('eclipticseasons:crops/dry_average', 'flavor_immersed_daily:puplesweetpotatoseed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:puplesweetpotatoseed');
+
+	event.add('eclipticseasons:crops/dry_average', 'flavor_immersed_daily:sesameseed');
+	event.add('eclipticseasons:crops/spring_summer', 'flavor_immersed_daily:sesameseed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:sweetgreenpepperseed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:sweetgreenpepperseed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:zucchiniseed');
+	event.add('eclipticseasons:crops/spring_summer', 'flavor_immersed_daily:zucchiniseed');
+
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:spinach_seed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:spinach_seed');
+
+	event.add('eclipticseasons:crops/moist_humid', 'flavor_immersed_daily:cauliflower_seed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:cauliflower_seed');
+
+	event.add('eclipticseasons:crops/dry_average', 'flavor_immersed_daily:scallion_seed');
+	event.add('eclipticseasons:crops/spring_autumn', 'flavor_immersed_daily:scallion_seed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:lilac_seed');
+	event.add('eclipticseasons:crops/spring_summer', 'flavor_immersed_daily:lilac_seed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:red_pepper_seed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:red_pepper_seed');
+	
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:sweet_potato_seed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:sweet_potato_seed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:si_chuan_pepper_seed');
+	event.add('eclipticseasons:crops/spring_summer_autumn', 'flavor_immersed_daily:si_chuan_pepper_seed');
+
+	event.add('eclipticseasons:crops/average_moist', 'flavor_immersed_daily:pea_nut_seed');
+	event.add('eclipticseasons:crops/spring_summer', 'flavor_immersed_daily:pea_nut_seed');
+});
